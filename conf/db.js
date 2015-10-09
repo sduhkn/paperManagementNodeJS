708be222d52744f5216c9a327835e606cc6cf38a
sdwww <1043443209@qq.com>
2015-10-09 14:50:18 +0800

    init database

--- conf/db.js
+++ conf/db.js
@@ -3,10 +3,10 @@
  */
 module.exports = {
     mysql: {
-        host: '127.0.0.1',
+        host: '211.87.239.153',
         user: 'root',
-        password: '123456',
-        database:'back', // 前面建的user表位于这个数据库中
+        password: 'dareway.DAREWAY',
+        database:'db_sde', // 前面建的user表位于这个数据库中
         port: 3306
     }
 };
