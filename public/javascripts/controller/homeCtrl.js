70ef92006983e5065fb52a5e0c2eafe02c2f92ff
Kangning Huang <sduhkn@163.com>
2015-10-11 21:30:02 +0800

    add factory1

--- public/javascripts/controller/homeCtrl.js
+++ public/javascripts/controller/homeCtrl.js
@@ -3,12 +3,35 @@
  */
 var home = angular.module("home", ['ui.router']);
 
+home.factory('paperService',function(){
+    var paperInfo = {};
+    function setPaperInfo(paper) {
+        paperInfo = paper;
+    }
+    function getPaperInfo() {
+        return paperInfo;
+    }
+
+    return {
+        setPaperInfo: setPaperInfo,
+        getPaperInfo: getPaperInfo
+    }
+});
+//ctrl the routes
 home.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when("", "/page1");
+    $urlRouterProvider.when("", "/showMyPaper");
     $stateProvider
-        .state("page1", {
-            url:"/page1",
-            templateUrl: "./views/stu/Page1.ejs",
+        .state("showMyPaper", {
+            url:"/showMyPaper",
+            views:{
+                '':{
+                    templateUrl: "./views/stu/showMyPaper.ejs",
+                }
+            }
+        })
+        .state("editPaper",{
+            url:'/editPaper',
+            templateUrl: './views/stu/editPaper.ejs',
         })
         .state("page2", {
             url:"/page2",
@@ -23,19 +46,26 @@ home.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl: "./views/stu/Page4.ejs"
         });
 });
-home.controller('stu_showPaperInfo',function($scope,$http){
-    $http.get('/myPaperInfo')
+//show the paper info
+//showMyPaper.ejs
+home.controller('stu_showPaperInfo',['$scope','$http','paperService',function($scope,$http,paperService){
+    $http.get('/stu/myPaperInfo')
         .success(function(data, status){
             $scope.paperInfo = data.paperInfo;
         }).error(function(data, status){
             alert("error: "+status);
         });
 
-    $scope.edit = function(paper){
-
+    $scope.editPaper = function(paper){
+        paperService.setPaperInfo(paper);
     }
-});
-//控制菜单点击样式
+}]);
+//edit and update the paper info
+//editPaper.ejs
+home.controller('editPaperInfo',['$scope','paperService',function($scope,paperService){
+    $scope.paper = paperService.getPaperInfo();
+}]);
+//ctrl css style of the right menu
 home.controller('navbar',function($scope){
     $("li[name='myli']").click(function(){
         $(this).addClass("active").siblings('li').removeClass("active");
