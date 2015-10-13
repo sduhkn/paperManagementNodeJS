1af4d7c4c908ab3a3d402b2536e7c169fb52e47a
sdwww <1043443209@qq.com>
2015-10-13 22:31:38 +0800

    finish changepwd editpaper editstuinfo

--- DAO/userDAO.js
+++ DAO/userDAO.js
@@ -4,98 +4,100 @@
 var mysql = require("mysql");
 var conf = require('../conf/db');
 var pool = mysql.createPool(conf.mysql);
+var crypto = require('crypto');
 
 console.log("DB connect successful");
 
 module.exports = {
-    queryAll: function(sql,callback){
-        pool.getConnection(function(err, conn){
-            if(err) return callback(err,null);
-            conn.query(sql,function(qerr, result){
+    queryAll: function (sql, callback) {
+        pool.getConnection(function (err, conn) {
+            if (err) return callback(err, null);
+            conn.query(sql, function (qerr, result) {
                 callback(qerr, result);
                 conn.release();
             });
         });
     },
-    add: function(sql,req,res,callback){
-        pool.getConnection(function(err, conn){
+    add: function (sql, req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var param = req.body || req.query;
-            conn.query(sql,[param.username,param.pwd],function(err,result){
-                callback(err,result);
+            conn.query(sql, [param.username, param.pwd], function (err, result) {
+                callback(err, result);
                 conn.release();
             });
         });
     },
-    checkLogin: function(req, res, callback){
-        pool.getConnection(function(err,conn){
+    checkLogin: function (req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var params = req.body;
             var sql = 'select sid,password from student_info where sid = ?';
-            conn.query(sql,params.username,function(err, result) {
-                if(result)
+            conn.query(sql, params.username, function (err, result) {
+                if (result)
                     callback(err, result);
             });
-
+            conn.release();
         });
     },
     // show user herself paper info
-    showPaperInfoQueryByID: function(sql, callback){
-        pool.getConnection(function(err,conn){
-            conn.query(sql, function(err, result) {
-                if(result)
+    showPaperInfoQueryByID: function (sql, callback) {
+        pool.getConnection(function (err, conn) {
+            conn.query(sql, function (err, result) {
+                if (result)
                     callback(err, result);
             });
+            conn.release();
         });
     },
-    showStuOwnInfoQueryByID: function(sql, callback){
-        pool.getConnection(function(err,conn){
-            conn.query(sql, function(err, result) {
-                if(result){
-                    callback(err, result);}
+    showStuOwnInfoQueryByID: function (sql, callback) {
+        pool.getConnection(function (err, conn) {
+            conn.query(sql, function (err, result) {
+                if (result) {
+                    callback(err, result);
+                }
 
             });
+            conn.release();
         });
     },
-    comparePassword: function(req, res, callback){
-        pool.getConnection(function(err,conn){
-            var params = req.body;
+    comparePassword: function (req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var sql = 'select password from student_info where sid = ?';
-            conn.query(sql,params.username,function(err, result) {
-                if(result)
+            conn.query(sql, req.cookies["userID"], function (err, result) {
+                if (result)
                     callback(err, result);
             });
-
+            conn.release();
         });
     },
-    changePassword: function(req, res, callback){
-        pool.getConnection(function(err,conn){
+    changePassword: function (req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var params = req.body;
-            var sql = 'update password =? from student_info where sid = ?';
-            conn.query(sql,params.password,params.username,function(err, result) {
-                if(result)
-                    callback(err, result);
+            var sha1_pwd = crypto.createHash('sha1').update(params.newPwd).digest("base64");
+            var sql = 'update student_info set password =? where sid = ?';
+            conn.query(sql, [sha1_pwd, req.cookies["userID"]], function (err, result) {
+                callback(err);
             });
-
+            conn.release();
         });
     },
-    showPaperIndex: function(req, res, callback){
-        pool.getConnection(function(err,conn){
+    updatePaperInfo: function (req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var params = req.body;
-            var sql = 'select password from student_info where sid = ?';
-            conn.query(sql,params.username,function(err, result) {
-                if(result)
-                    callback(err, result);
+            var sql = 'update paper_info set title =? where paperid = ?';
+            conn.query(sql, [params.title, params.paperid], function (err, result) {
+                callback(err);
             });
-
+            conn.release();
         });
     },
-    queryOwnPaper: function(req, res, callback){
-        pool.getConnection(function(err,conn){
+    updateStuInfo: function (req, res, callback) {
+        pool.getConnection(function (err, conn) {
             var params = req.body;
-            var sql = 'select password from student_info where sid = ?';
-            conn.query(sql,params.username,function(err, result) {
-                if(result)
-                    callback(err, result);
+            var sql = 'update student_info set sex =? ,sname=? where sid = ?';
+            conn.query(sql, [params.sex, params.sname,req.cookies['userID']], function (err, result) {
+                callback(err);
             });
+            conn.release();
         });
     },
 };
--- routes/stu/stuRoute.js
+++ routes/stu/stuRoute.js
@@ -4,12 +4,55 @@
 var express = require('express');
 var router = express.Router();
 var userDAO = require('../../DAO/userDAO');
+var crypto = require('crypto');
 // get the own paperInfo
-router.get('/myPaperInfo',function(req, res){
+router.get('/myPaperInfo', function (req, res) {
     var sql = "select * from paper_info where fauthor = " + req.cookies["userID"];
-    userDAO.showPaperInfoQueryByID(sql,function(err, result){
-        res.send({paperInfo: result });
+    userDAO.showPaperInfoQueryByID(sql, function (err, result) {
+        res.send({paperInfo: result});
     });
 });
+router.get('/stuOwnInfo', function (req, res) {
+    var sql = "select sid,sname,sex from student_info where sid = " + req.cookies["userID"];
+    userDAO.showStuOwnInfoQueryByID(sql, function (err, result) {
+        res.send({stuInfo: result[0]});
+    });
+});
+router.post('/changePassword', function (req, res) {
+    //console.log(req.body.oldPwd);
+    userDAO.comparePassword(req, res, function (err, result) {
+        if (result[0].password != crypto.createHash('sha1').update(req.body.oldPwd).digest("base64")) {
+            res.send({msg: "原密码不正确"});
+        }
+        else {
+            userDAO.changePassword(req, res, function (err) {
+                if (err) {
+                    res.send({msg: "未知错误"});
+                }
+                else
+                    res.send({msg: "修改成功"});
+            })
+        }
+    })
 
+});
+router.post('/updatePaperInfo', function (req, res) {
+    //console.log(req.body.oldPwd);
+    userDAO.updatePaperInfo(req, res, function (err) {
+        if (err) {
+            res.send({msg: "未知错误"});
+        }
+        else
+            res.send({msg: "修改成功"});
+    })
+});
+router.post('/updateStuInfo', function (req, res) {
+    userDAO.updateStuInfo(req, res, function (err) {
+        if (err) {
+            res.send({msg: "未知错误"});
+        }
+        else
+            res.send({msg: "修改成功"});
+    })
+});
 module.exports = router;
